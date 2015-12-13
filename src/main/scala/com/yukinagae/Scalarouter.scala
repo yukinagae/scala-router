6400eb390870afe0556aafeb9eae2c4c9e416004
Yuki Nagae <yuki.nagae1130@gmail.com>
2015-12-13 16:35:45 +1100

    indentation is 2 spaces

--- src/main/scala/com/yukinagae/Scalarouter.scala
+++ src/main/scala/com/yukinagae/Scalarouter.scala
@@ -2,53 +2,54 @@ package com.yukinagae
 
 object ScalaRouter {
 
-	type Action = Map[String, String] =>  Any
+  type Action = Map[String, String] =>  Any
 
   def routes(rs: Seq[(Method, String, Action)]): Request => Response = {
-  	(request: Request) => {
-  		val params = buildParams(request)
-			val response = rs.find { r =>
-				methodMatched(request, r._1) && pathMatched(request, r._2)._1
-				} match {
-				case Some(best) => {
-					val result = pathMatched(request, best._2) // TODO calling the same method twice!
-					val ps = params ++ result._2.getOrElse(Map.empty)
-					val body = best._3(ps).toString // TODO should be converted regarding Types
-					Response(200, Map("Content-Type" -> "text/plain"), body)
-				}
-				case None => NOT_FOUND(request)
-			}
-			response
-		}
-	}
+    (request: Request) => {
+      val params = buildParams(request)
+      val response = rs.find { r =>
+        methodMatched(request, r._1) && pathMatched(request, r._2)._1
+      } match {
+        case Some(best) => {
+          val result = pathMatched(request, best._2) // TODO calling the same method twice!
+	  val ps = params ++ result._2.getOrElse(Map.empty)
+	  val body = best._3(ps).toString // TODO should be converted regarding Types
+	  Response(200, Map("Content-Type" -> "text/plain"), body)
+        }
+        case None => NOT_FOUND(request)
+      }
+      response
+    }
+  }
 
-	def methodMatched(request: Request, targetMethod: Method): Boolean = targetMethod == Method.ANY || request.requestMethod == targetMethod.name
+  def methodMatched(request: Request, targetMethod: Method): Boolean = targetMethod == Method.ANY || request.requestMethod == targetMethod.name
 
-	def pathMatched(request: Request, targetPath: String): (Boolean, Option[Map[String, String]]) = {
-		val result = Scalout.routeMatches(targetPath, request)
-		result
-	}
+  def pathMatched(request: Request, targetPath: String): (Boolean, Option[Map[String, String]]) = {
+    val result = Scalout.routeMatches(targetPath, request)
+    result
+  }
 
-	def buildParams(r: Request): Map[String, String] = {
-		if(r.URI.contains('?')) {
-			val a = r.URI.split('?').last.split('&')
-			val map = a.map(e => e.split("=").head -> e.split("=").last).toMap
-			map
-		} else {
-			Map.empty	
-		}
-	}
+  def buildParams(r: Request): Map[String, String] = {
+    if(r.URI.contains('?')) {
+      val a = r.URI.split('?').last.split('&')
+      val map = a.map(e => e.split("=").head -> e.split("=").last).toMap
+      map
+    } else {
+      Map.empty
+    }
+  }
 
-	def NOT_FOUND(request: Request): Response = Response(400, Map("Content-Type" -> "text/plain"), "Not Found")
+  def NOT_FOUND(request: Request): Response = Response(400, Map("Content-Type" -> "text/plain"), "Not Found")
 
 }
 
 object Method {
-	case object GET extends Method("GET")
-	case object POST extends Method("POST")
-	case object ANY extends Method("ANY")
+  case object GET extends Method("GET")
+  case object POST extends Method("POST")
+  case object ANY extends Method("ANY")
 }
 
 sealed abstract class Method(val name: String) {
     // val name = name
 }
+
