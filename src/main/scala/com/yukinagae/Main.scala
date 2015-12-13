4592b6f878c26b0239fda23d052a6bf19389c5a5
Yuki Nagae <yuki.nagae1130@gmail.com>
2015-12-13 16:57:54 +1100

    get :id from url

--- src/main/scala/com/yukinagae/Main.scala
+++ src/main/scala/com/yukinagae/Main.scala
@@ -1,28 +1,38 @@
 package com.yukinagae
 
-import com.yukinagae.Method.GET
+import com.yukinagae.Method.{ GET, POST }
 
 object Main extends App {
 
   def index(ps: Map[String, String]): String = {
     println(ps)
-    "index"
+    s"""
+    <h1>index page</h1>
+    <form method="POST" action="/">
+      <button type="submit">new</button>
+    </form>
+    """
   }
 
-  def foo(ps: Map[String, String]): String = {
+  def show(ps: Map[String, String]): String = {
     println(ps)
-    "foo"
+    s"""
+    <h1>show page</h1>
+    <p>${ps.get(":id").get}</p>
+    """
   }
 
-  def bar(ps: Map[String, String]): String = {
+  def create(ps: Map[String, String]): String = {
     println(ps)
-    "bar"
+    s"""
+    <h1>create page</h1>
+    """
   }
 
   val routes = ScalaRouter.routes(Seq(//
     (GET, "/", index),//
-    (GET, "/:x", foo),//
-    (GET, "/bar", bar)))
+    (GET, "/:id", show),//
+    (POST, "/", create)))
     
     JettyAdapter.run(routes)
 
